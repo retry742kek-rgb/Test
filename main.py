4d215139f217af44ac594f4871d8887504ca0bf8
Anastasya-sudo <stasya-orient@yandex.ru>
2025-09-25 11:47:17 +0300

    Add calculator

---
+++ main.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = int(input("Введите первое число: "))
+    y = int(input("Введите второе число: "))
+    print("Сумма:", add(x, y))
